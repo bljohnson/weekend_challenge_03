e9c0e993c4058544ad4eb814bd614edbe719b102
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-13 13:24:41 -0500

    Clarified parameter in ajax success function doesn't correspond to data: inputObject

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // populates req.body, allows us to hide data when posting
 var calculateStuff = require('../modules/calculation.js');
 
-var urlencodedParser = bodyParser.urlencoded({extended:false});
+var urlencodedParser = bodyParser.urlencoded({extended:false});// populates req.body, parses urlencoded bodies only. new body object containing parsed data is populated on request object. object contains key-value pairs where value can be string or array (since extended: false)
 
 var server = app.listen(process.env.PORT||3000, function() {
   console.log('listening on port');
