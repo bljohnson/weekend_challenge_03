2605f5b38f646fe4a110ec279a24433078e90312
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-13 15:49:30 -0500

    Add operation buttons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,41 @@
+var doThis = '+'; // set doThis as global variable, set to 'add' to set a default operation in case user forgets to choose one
+
 $(document).ready(function() {
 
   console.log("hello from jquery");
+
+  // testing operation Buttons
+  $('#addItUp').on('click', function(){
+    doThis = '+';
+  });
+  $('#subtractIt').on('click', function(){
+    doThis = '-';
+  });
+  $('#multiplyIt').on('click', function(){
+    doThis = '*';
+  });
+  $('#divideIt').on('click', function(){
+    doThis = '/';
+  });
+  // end testing buttons
 
   $('#submit').on('click', function(event) {
     event.preventDefault();
     startServerSideOperation();
   }); // end submit button function
 
-  $('#clear').on('click', function(event) {
+  $('#clear').on('click', function(event) { // yay this works with the new operation buttons!!
     event.preventDefault();
     $('#firstNum').val(''); // clears first input field
     $('#secondNum').val(''); // clears second input field
-    $('.math').prop('selectedIndex', 0); // resets select dropdown menu to "Select..." which is the first index of the select menu
+    // $('.math').prop('selectedIndex', 0); // resets select dropdown menu to "Select..." which is the first index of the select menu
     $('#outputDiv').empty(); // clear div containing answer
   }); // end clear button function
 
 }); // end doc ready function
 
+
+// global functions/variables below
 
 var processResponse = function (response) {
   console.log('in processResponse: ' + response);
@@ -26,24 +45,24 @@ var processResponse = function (response) {
 
 
 // create object from form input and send request to server
-function startServerSideOperation () {
+function startServerSideOperation () { // this is a lot of stuff - best to extract into a function and call the function where you want all this to happen
   // console.log( 'in startServerSideOperation' );
-  // assemble object from input
+  // assemble object from input. don't have to set as new vars, can put the stuff after the = in inputObject, but looks cleaner to do it this way
   var firstNumber = $('#firstNum').val();
   var secondNumber = $('#secondNum').val();
-  var mathOperation = $('.math option:selected').text();
+  var mathOperation = doThis;
 
   var inputObject = {
     "input1": firstNumber,
     "input2": secondNumber,
-    "doThis": mathOperation
+    "oper": mathOperation
   }; // end inputObject
 
   // post to server with ajax;
-  $.ajax({
+  $.ajax({ // this is a method call to AJAX in jQuery that receives an object
    url: "/pathPost",
-   type: "POST",
-   data: inputObject,
+   type: "POST", // makes request to server, server posts this ajax call to above url using app.post. if type were GET, would make request to server, server would get this ajax call from above url.
+   data: inputObject, // what gets sent to the server
    success: function(newData){ // represents response from server (calculation completed in server node module that gets passed in app.post as res.write or res.send)
           console.log(newData);
   //         // if post is successful we've received back "newData"
--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser'); // populates req.body, allows us to hide data when posting
+var bodyParser = require('body-parser'); // required for POSTing; populates req.body, allows us to hide data when posting
 var calculateStuff = require('../modules/calculation.js');
 
-var urlencodedParser = bodyParser.urlencoded({extended:false});// populates req.body, parses urlencoded bodies only. new body object containing parsed data is populated on request object. object contains key-value pairs where value can be string or array (since extended: false)
+var urlencodedParser = bodyParser.urlencoded({extended:false});// required fpr POSTing; populates req.body, parses urlencoded bodies only. new body object containing parsed data is populated on request object. object contains key-value pairs where value can be string or array (since extended: false)
 
 var server = app.listen(process.env.PORT||3000, function() {
   console.log('listening on port');
@@ -19,6 +19,6 @@ app.get('/', function (req, res) {
 app.post('/pathPost', urlencodedParser, function(req,res) {
   var uno = Number(req.body.input1);
   var dos = Number(req.body.input2);
-  res.write('Answer: ' + calculateStuff(uno, dos, req.body.doThis));
+  res.write('Answer: ' + calculateStuff(uno, dos, req.body.oper)); // this represents the contents of the response parameter in client.js. req.body.oper is a string, so don't need to conver to Number and then back to string
   res.end();
 }); // end of /pathPost app.post
